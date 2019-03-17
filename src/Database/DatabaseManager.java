762f96ae188d36115a4da63e3a5f51cea9f04162
Sigmund Høeg <sigmunhh@stud.ntnu.no>
2019-03-17 10:01:58 +0100

    Begins the implementation of a simple method that gets all th exersices

--- src/Database/DatabaseManager.java
+++ src/Database/DatabaseManager.java
@@ -1,6 +1,31 @@
 package Database;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class DatabaseManager {
-	private static String PATH = "", DATABASE_NAME="", USERNAME = "", PASSWORD = "";
-
+	private static final String DB_DRIVER_PATH = "com.mysql.cj.jdbc.Driver", 
+			DB_NAME="", DB_USERNAME = "", DB_PASSWORD = "", 
+			CONNECTION_STRING = "jdbc:mysql://mysql.stud.ntnu.no/" + DB_NAME + "?serverTimezone=UTC";
+	
+	private static Connection connection;
+	
+	
+	static { 
+		openConnection();
+	}
+	
+	private static void openConnection() {
+			try {
+				connection = DriverManager.getConnection(CONNECTION_STRING, DB_USERNAME, DB_PASSWORD);
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	
+	}
 }
