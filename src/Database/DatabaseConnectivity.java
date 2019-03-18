ab9352942c4e6c915c25de7d29a8c30563cc5017
Kristoffer Rakstad Solberg <solkristoffer@gmail.com>
2019-03-18 17:06:31 +0100

    added open and close connection

--- src/Database/DatabaseConnectivity.java
+++ src/Database/DatabaseConnectivity.java
@@ -13,7 +13,7 @@ class MysqlCon{
 	private static Connection con;
 	private static ResultSet rs;
 
-	// Function for executing query
+	// Function for executing
 	protected static ResultSet executeMyQuery(String query) {
 
 		try{  
@@ -27,11 +27,11 @@ class MysqlCon{
 		// print every attribute
 		while(rs.next())  
 			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
-			con.close();  
 
 		// if query fails	
-		}catch(
-			Exception e){ System.out.println(e);
+		}catch(Exception e){ 
+			System.out.println(e);
+
 		} 
 
 		// return result
@@ -39,19 +39,45 @@ class MysqlCon{
 	}
 
 
-	// Main
-	public static void main(String args[]){  
-
+	// Open SQL connection
+	private static void openConnection() {
 		try{  
 			// Find drivers
 			Class.forName(DB_DRIVER_PATH);  
 
 			// Mabye 'con' is only specified locally. Can cause error
 			con=DriverManager.getConnection(CONNECTION_STRING,USER,PASS);  
-		}catch(Exception e){ System.out.println(e);}  
+
+		}catch(Exception e){
+		 System.out.println(e);
+		}  		
+	}
 
 
+	// Close SQL connection
+	private static void closeConnection(){
+
+		try{
+			// close connection to database
+			con.close(); 
+
+		}catch(Exception e){
+		 System.out.println(e);
+		}
+	}
+
+
+
+	// Main
+	public static void main(String args[]){
 		// test
-		executeMyQuery("SELECT * FROM Exercise");
+		try{
+			openConnection();
+			executeMyQuery("SELECT * FROM Exercise");
+			closeConnection();
+
+		}catch(Exception e){
+			System.out.println(e);
+		} 
 	}  
-}  	
+}  		
