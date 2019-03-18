fed36ca986df0e0c3be3a2402a7f5559f8e65d7a
Kristoffer Rakstad Solberg <solkristoffer@gmail.com>
2019-03-18 16:42:25 +0100

    executequery works with input argument

--- src/Database/DatabaseConnectivity.java
+++ src/Database/DatabaseConnectivity.java
@@ -3,25 +3,39 @@ import java.sql.*;
 
 class MysqlCon{  
 
+	// Variables for connecting to database
 	private static final String DB_DRIVER_PATH = "com.mysql.jdbc.Driver";
 	private static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/workoutLog?autoReconnect=true&useSSL=false";
 	private static final String USER = "root";
 	private static final String PASS = "password";
 
+	// Variables for executing queries
+	private static Connection con;
+	private static ResultSet rs;
 
+	// Function for executing query
 	protected static ResultSet executeMyQuery(String query) {
 
 		try{  
-			 
-			Statement stmt=con.createStatement();  
-			ResultSet rs=stmt.executeQuery(query);  
 
+			// Prepare your statement 
+			Statement stmt=con.createStatement();  
+			
+			// Execute query and return object
+			rs=stmt.executeQuery(query);  
+
+		// print every attribute
 		while(rs.next())  
 			System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
 			con.close();  
-		}catch(Exception e){ System.out.println(e);} 
 
-		Return ResultSet;
+		// if query fails	
+		}catch(
+			Exception e){ System.out.println(e);
+		} 
+
+		// return result
+		return rs;
 	}
 
 
@@ -32,12 +46,12 @@ class MysqlCon{
 			// Find drivers
 			Class.forName(DB_DRIVER_PATH);  
 
-			//here 'workoutLog' is database name, 'root' is username and passwod is 'password' 
 			// Mabye 'con' is only specified locally. Can cause error
-			Connection con=DriverManager.getConnection(CONNECTION_STRING,USER,PASS);  
+			con=DriverManager.getConnection(CONNECTION_STRING,USER,PASS);  
 		}catch(Exception e){ System.out.println(e);}  
 
 
-		executeQuery("SELECT * FROM Exercise");
+		// test
+		executeMyQuery("SELECT * FROM Exercise");
 	}  
 }  	
