9771fcb74ad46ce8dbe9b9e615214df89046be62
Petter Sletsjøe <petter.sletsjoe@gmail.com>
2019-03-19 17:05:02 +0100

    add data classes and data handler

---
+++ src/classes/Workout.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Workout {
+  String name;
+  String description;
+  String date;
+  String time;
+  String duration;
+  Int workoutFeat;
+  List<Exercise> exercisesInWorkout;
+  List<ExerciseInfo> exerciseInWorkout;
+}
